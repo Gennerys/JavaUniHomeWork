3c972f9b9862fe7f17232f7d30e7d77bde71c108
Alexander Papirnyk <43519694+Gennerys@users.noreply.github.com>
2020-02-23 15:23:07 +0300

    Completed HomeWork2

---
+++ HomeWork2/Calculator_Interfaces/src/main/java/ICalculator.java
@@ -0,0 +1,9 @@
+public interface ICalculator {
+    double addition(double firstOperand, double secondOperand);
+
+    double subtraction(double firstOperand, double secondOperand);
+
+    double multiplication(double firstOperand, double secondOperand);
+
+    double division(double firstOperand, double secondOperand);
+}
